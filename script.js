0a14dd6b08313a353d5246a1594fca56a705efa6
Mateusz Zaniewski <m.zaniewski1995@gmail.com>
2021-06-20 21:57:52 +0200

    add scroll smooth

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 const navWrapper = document.querySelector('.nav__bar');
 const navToggle = document.querySelector('.nav__toggle');
 navToggle.addEventListener("click", ()=>{
-    navWrapper.classList.toggle('enable')
+    navWrapper.classList.toggle('nav__bar--active')
     navToggle.classList.toggle('nav__toggle--active')
 })
+
+
+
+
+
+
+document.querySelector(".footer__go-up").addEventListener('click', ()=>{           
+    const scrollTop = window.setInterval(function() {
+        const topPosition = window.pageYOffset;
+        if ( topPosition > 0 ) 
+        {
+            window.scrollTo( 0, topPosition - 20 ); 
+        } 
+        else 
+        {
+            window.clearInterval( scrollTop );
+        }
+    }, ); 
+    })
+    
