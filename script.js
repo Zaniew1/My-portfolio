0d445f751fb3554f53600224a21347ecc98e25e4
Mateusz Zaniewski <m.zaniewski1995@gmail.com>
2021-06-30 18:17:35 +0200

    add comments in script.js

--- script.js
+++ script.js
@@ -1,39 +1,29 @@
 const navWrapper = document.querySelector('.nav__bar');
 const navToggle = document.querySelector('.nav__toggle');
 navToggle.addEventListener("click", ()=>{
-    navWrapper.classList.toggle('nav__bar--active')
+    navWrapper.classList.toggle('nav__bar--active')         //funkcja otwiera i zamyka navbar po naciśnięciu ikony burgera
     navToggle.classList.toggle('nav__toggle--active')
 })
 
-
-
-
-
-
-
-
-    
-
-
-    const navLinks = [...document.querySelectorAll('.nav__link')];
-    const homePosition = document.querySelector('.indexWrapper').offsetTop;
-    const aboutPosition = document.querySelector('.about').offsetTop;
-    const techPosition = document.querySelector('.tech').offsetTop;
-    const projectsPosition = document.querySelector('.projects').offsetTop;
-    const contactPosition = document.querySelector('.contact').offsetTop;
-    const lookMyProjects = document.querySelector(".front__button");
-    const goUpButton = document.querySelector(".footer__go-up");
+    const navLinks = [...document.querySelectorAll('.nav__link')];                  // wszystkie linki w nawigacji - jako tablica
+    const homePosition = document.querySelector('.indexWrapper').offsetTop;         // górna krawędź sekcji home
+    const aboutPosition = document.querySelector('.about').offsetTop;               // górna krawędź sekcji about
+    const techPosition = document.querySelector('.tech').offsetTop;                 // górna krawędź sekcji tech
+    const projectsPosition = document.querySelector('.projects').offsetTop;         // górna krawędź sekcji projects
+    const contactPosition = document.querySelector('.contact').offsetTop;           // górna krawędź sekcji contact
+    const lookMyProjects = document.querySelector(".front__button");                // button "zobacz moje projekty"
+    const goUpButton = document.querySelector(".footer__go-up");                    // button zabierający na początek strony 
 
     const removeNav = ()=>{
-        navWrapper.classList.remove('nav__bar--active')
+        navWrapper.classList.remove('nav__bar--active')             // funkcja chowająca nawigację 
         navToggle.classList.remove('nav__toggle--active')
     }
 
 
     navLinks.forEach((navLink, index ) =>{
-        navLink.addEventListener('click', ()=>{
+        navLink.addEventListener('click', ()=>{                 // funkcja scrollująca stronę do danej sekcji po naciśnięciu linka w nawigacji
 
-            if(index === 0){
+            if(index === 0){                                    
                 const scrollTo = window.setInterval(function(){
                     window.scrollTo( 0, homePosition - 20 ); 
                     window.clearInterval( scrollTo );          
@@ -68,21 +58,21 @@ navToggle.addEventListener("click", ()=>{
                 },6 ); 
             }
             
-            lookMyProjects.addEventListener('click', ()=>{  
+            lookMyProjects.addEventListener('click', ()=>{              //funkcja scrollująca do projektów po naciśnięciu buttona "zobacz moje projekty"
                 const scrollTo = window.setInterval(function(){
                     window.scrollTo( 0, projectsPosition - 20 ); 
                     window.clearInterval( scrollTo );          
                 },6 ); 
             })
 
-            goUpButton.addEventListener('click', ()=>{           
+            goUpButton.addEventListener('click', ()=>{                      //funkcja scrollująca do projektów po naciśnięciu buttona - strzałka do góry
                 const scrollTo = window.setInterval(function(){
                     window.scrollTo( 0, topPosition - 20 ); 
                     window.clearInterval( scrollTo );
                 },6 ); 
                 })
 
-            removeNav();
+            removeNav();        // funkcja chowająca nawigację po naciśnięciu jakiegokolwiek linka 
         })
     })
 
@@ -102,18 +92,19 @@ navToggle.addEventListener("click", ()=>{
         contactLink.classList.remove('color');
     }
 
-    const colorNavChange = ()=>{
-        const topPosition = window.pageYOffset;
-            if(topPosition <= aboutPosition){
-                colorNavRemove();
+    const colorNavChange = ()=>{            // funkcja dająca kolor linkom w nawigacji zależnie od miejsca w którym jesteśmy na stronie 
+        const topPosition = window.pageYOffset;             // na jakiej wysokości obecnie jest krawędź górnego ekranu
+        const navHeight = document.querySelector('.nav__bar').clientHeight;     // wysokość nav baru 
+            if(topPosition <= (aboutPosition - navHeight)){         // jeżeli górna krawędź ekranu jest między górą dokumentu a miejscem w którym się zaczyna sekcja about (minus wysokość navbaru)
+                colorNavRemove();                                   // przed każdym nadaniem koloru danemu linkowi, zerujemy wszystkie linki, aby nie było dwóch naraz 
                 homeLink.classList.add('color');
-            } else if(topPosition <= techPosition){
+            } else if(topPosition <= (techPosition - navHeight)){
                 colorNavRemove();
                aboutLink.classList.add('color');
-            } else if(topPosition <= projectsPosition){
+            } else if(topPosition <= (projectsPosition - navHeight)){
                 colorNavRemove();
                 techLink.classList.add('color');
-            } else if(topPosition <= contactPosition){
+            } else if(topPosition <= (contactPosition - navHeight)){
                 colorNavRemove();
                 projectsLink.classList.add('color');
             } else {
@@ -121,4 +112,4 @@ navToggle.addEventListener("click", ()=>{
                 contactLink.classList.add('color');
             }
     }
-    document.addEventListener('scroll', colorNavChange);
+    document.addEventListener('scroll', colorNavChange);            // wywołanie na scrollu
